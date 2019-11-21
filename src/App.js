ddc404899c6dbaae14e41cefea542cb5171db8a9
dseong0224 <dseong0224@gmail.com>
2019-11-21 19:06:51 +0000

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './components/Form';
+
+class App extends Component {
+  getRecipe (e){
+    e.preventDefault();
+    console.log("Working");
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">CELP for Cooking Help</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+      </div>
+    )
+  }
 }
 
 export default App;
