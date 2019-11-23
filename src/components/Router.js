45c6ebc1fe10616322ad95070b3d7f7ba462be60
dseong0224 <dseong0224@gmail.com>
2019-11-23 19:24:22 +0000

    set error page set load screen

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from '../App';
 import Recipe from "./Recipe";
+import Error from './Error';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/recipe/:id" component={Recipe} />
+      <Route component={Error} />
     </Switch>
   </BrowserRouter>
 )
