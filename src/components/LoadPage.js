bf83ea6194a3994e37de22acad21aea6b33c15f0
dseong0224 <dseong0224@gmail.com>
2019-11-23 19:54:49 +0000

    changed some styling for recipe page

---
+++ src/components/LoadPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const LoadPage = () => {
+  return (
+    <div className="row">
+      <h2>Create your snippet's HTML, CSS and Javascript in the editor tabs</h2>
+      <div id="loading">
+        <ul className="bokeh">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default LoadPage; 
