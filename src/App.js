edb397a7ea8b4a9e1cc95e965d83c398acbe8a20
dseong0224 <dseong0224@gmail.com>
2019-11-23 06:39:50 +0000

    achieved mvp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Form from './components/Form';
+import Recipes from './components/Recipes';
 
 const APP_ID = "34d187f5";
 const APP_API = "91723cb18ae377899fd99c366f7f5099"
@@ -12,12 +13,12 @@ class App extends Component {
   state = {
     recipes: []
   }
-  
+
   getRecipe = async (e) => {
     e.preventDefault();
     const recipeName = e.target.elements.recipeName.value
-    const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_API}`) ;
-    
+    const api_call = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_API}`);
+
     const data = await api_call.json();
     const recipes = data.hits
     this.setState({
@@ -25,19 +26,28 @@ class App extends Component {
     })
     console.log("data: ", this.state.recipes);
   }
+
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ 
+      recipes 
+    })
+  }
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">CELP for Cooking Help</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
-        { this.state.recipes.map( recipe => (
-        <div key={recipe.recipe.label}>
-          <img src={recipe.recipe.image} alt={recipe.recipe.label}/>
-          <p>{recipe.recipe.label}</p>
-        </div>
-        ) ) }
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     )
   }
