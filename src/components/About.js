4892fb18c2b6d01a819342de8edcc29aa6a23a95
Daniel Seong <dseong0224@gmail.com>
2019-12-17 21:26:44 -0800

    basic styling for about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,17 @@ import React from 'react';
 const About = () => {
   return (
     <div className="container">
-      <h1>Recipe Application using food API from Edamam</h1>
-      <p>Created by <strong>DANIEL SEONG</strong></p>
-      <ul>
+      <h1>Recipe Application using Edamam API</h1>
+      <h3>Created by<br></br> <strong>DANIEL SEONG</strong></h3>
+      <ul className="app-detail-list">
         <li>Recipe Application</li>
         <li>Used React Router to organized pages</li>
         <li>Navigation Bar for easy page navigation</li>
         <li>Clean, simple UI for easy use</li>
         <li>REST food API from Edamam.com</li>
       </ul>
-      <h5>
-        Please visit http://danielcseong.com or reach out to me directly via dseongdev@gmail.com or https://www.linkedin.com/in/dseongdev/
+      <h5 className="contact-me">
+        Please visit <a href="http://danielcseong.com">danielcseong.com</a> or reach out to me directly via <a href="mailto:dseongdev@gmail.com">dseongdev@gmail.com</a> or <a href="https://www.linkedin.com/in/dseongdev/">linkedin.com/in/dseongdev/</a>
       </h5>
     </div>
   )
