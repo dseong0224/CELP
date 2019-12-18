2b45a00476cda6796bbbe53a0d7b8ef41af264d1
Daniel Seong <dseong0224@gmail.com>
2019-12-17 20:55:07 -0800

    removed shop section added content to about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <p>About</p>
+    <div className="container">
+      <h1>Recipe Application using food API from Edamam</h1>
+      <p>Created by <strong>DANIEL SEONG</strong></p>
+      <ul>
+        <li>Recipe Application</li>
+        <li>Used React Router to organized pages</li>
+        <li>Navigation Bar for easy page navigation</li>
+        <li>Clean, simple UI for easy use</li>
+        <li>REST food API from Edamam.com</li>
+      </ul>
+      <h5>
+        Please visit http://danielcseong.com or reach out to me directly via dseongdev@gmail.com or https://www.linkedin.com/in/dseongdev/
+      </h5>
     </div>
   )
 }
